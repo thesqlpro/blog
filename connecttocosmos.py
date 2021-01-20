c7259d825a0b72d2ed5f87a0cf55ee356a73b0f7
Ayman El-Ghazali <46201919+thesqlpro@users.noreply.github.com>
2021-01-20 15:35:11 -0500

    big changes

--- connecttocosmos.py
+++ connecttocosmos.py
@@ -6,26 +6,17 @@ import connectioninfo as ci
 # Initialize the Cosmos client
 endpoint = ci.endpoint
 key = ci.key
-##endpoint = "https://aymancosmos.documents.azure.com:443/"
-##key = 'wwHJYw8GUy3078ZDjoUWOd28qrkVUIX2DDbPhVFUESnVEgCbTTGNPZFWLVqVTu9jrAlMjUDklkulUgzUZ5bafg=='
-
-# <create_cosmos_client>
+database_name = ci.database_name
+container_name = ci.container_name
 client = CosmosClient(endpoint, key)
-# </create_cosmos_client>
 
-# Create a database
-# <create_database_if_not_exists>
-##database_name = 'AzureSampleFamilyDatabase'
-database_name = 'testjsonpython'
+print("Database Name: " + database_name)
 database = client.create_database_if_not_exists(id=database_name)
-# </create_database_if_not_exists>
 
-# Create a container
-# Using a good partition key improves the performance of database operations.
-# <create_container_if_not_exists>
-container_name = 'zipcodes'
+print("Container Name: " + container_name)
 container = database.create_container_if_not_exists(
     id=container_name, 
     partition_key=PartitionKey(path="/id"),
     offer_throughput=400
 )
+
--- getzipdatacosmos.py
+++ getzipdatacosmos.py
@@ -1,37 +1,16 @@
 from azure.cosmos import exceptions, CosmosClient, PartitionKey
+import connecttocosmos as c2c
 
-# Initialize the Cosmos client
-endpoint = "https://aymancosmos.documents.azure.com:443/"
-key = 'wwHJYw8GUy3078ZDjoUWOd28qrkVUIX2DDbPhVFUESnVEgCbTTGNPZFWLVqVTu9jrAlMjUDklkulUgzUZ5bafg=='
-
-# <create_cosmos_client>
-client = CosmosClient(endpoint, key)
-# </create_cosmos_client>
-
-# Create a database
-# <create_database_if_not_exists>
-##database_name = 'AzureSampleFamilyDatabase'
-database_name = 'testjsonpython'
-database = client.create_database_if_not_exists(id=database_name)
-# </create_database_if_not_exists>
-
-# Create a container
-# Using a good partition key improves the performance of database operations.
-# <create_container_if_not_exists>
-container_name = 'zipcodes'
-container = database.create_container_if_not_exists(
-    id=container_name, 
-    partition_key=PartitionKey(path="/id"),
-    offer_throughput=400
-)
+database = c2c.database
+container = c2c.container
 
 ##Final query
-query = "SELECT * FROM c where c.state ='VA'"
+query = "SELECT * FROM c where c.state ='MD'"
 items = list(container.query_items(
     query=query,
     enable_cross_partition_query=True
 ))
 
 request_charge = container.client_connection.last_response_headers['x-ms-request-charge']
-
+print(query)
 print('Query returned {0} items. Operation consumed {1} request units'.format(len(items), request_charge))
